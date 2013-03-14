55de0f1a2c191449e263f9de886f53c60120c1b7
hugomarin <hm@hugomarin.com>
2013-03-13 20:05:33 -0500

    traducción

--- view/finalizar.php
+++ view/finalizar.php
@@ -11,7 +11,7 @@
 				<div class="twelve columns">
 					<h3>Gracias por participar en la convocatoria de <strong> artBO 2013.</strong></h3> 
 					
-					<p>En breve, recibira un correo electrónico de confirmación. de su proceso de registro.</p>					
+					<p>En breve, recibirá un correo electrónico de confirmación. de su proceso de registro.</p>					
 				</div>
 			</div>
 			<?php
@@ -31,10 +31,7 @@
 			</div>
 		</div><!--/inner-footer-->
 	</div><!-- END Main: Panel -->
-	<div class="advisory">
-		<span>Recomendamos visualizar en: IE 9.0 - Firefox 10.0 - Safari 5.1 - Chrome 17.0     |     Optimizada 1024 x 768</span>
-		<span><a href="http://www.artboonline.com/documentos/2129_reglamento_convocatoria_artecamara2013.pdf" target="_blank">Términos y Condiciones</a> del Sitio</span>
-	</div>
+	
 </div>
 	<!-- 3. END Row main -->
 
