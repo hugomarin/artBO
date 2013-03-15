74514361cbfdc63e2351e4f466c42d0d0f39718a
hugomarin <hm@hugomarin.com>
2013-03-15 18:26:46 -0500

    Endmail inglés

--- en/view/endmail2.php
+++ en/view/endmail2.php
@@ -1,33 +1,38 @@
 <?php
 $dir			= 'resources/galerias/'. $user->__get('user_id'). '-' .  makeUrlClear(utf8_decode($user->__get('user_name'))).'/';
-$html			= '';
+$html			= '<img src="http://www.activemgmd.com/ccb/ccb-galerias/images/logo-bw.jpg">';
 // FIRST
 $imagenGaleria	= APPLICATION_FULL_URL.$dir.$user->__get('user_gallery_image');
 $imagenDirector	= APPLICATION_FULL_URL.$dir.$user->__get('user_director_image');
 $country		= new Country('country_id');
 
 $html	.= "<h3>Gallery</h3>
+
+<br>
+<h1>artBO 2013 Application</h1>
+<br>
+<br>
 <p>
-Image gallery: <em>$imagenGaleria</em><br>
-Gallery name: <em> ".$user->__get('user_gallery_comname')." </em><br>
-Gallery name: <em> ".$user->__get('user_gallery_razon')." </em><br>
-Document type: <em> ".$user->__get('user_document_type')." </em><br>
-Document number: <em> ".$user->__get('user_gallery_document')." </em><br>
-Country: <em>".$country->__get('country_id')."</em><br>
-City: <em> ".$user->__get('user_city')." </em><br>
-Address: <em> ".$user->__get('user_address')." </em><br>
-ZIP: <em> ".$user->__get('user_postal_code')." </em><br>
-Gallery Review: <em> ".$user->__get('user_gallery_comname')." </em><br>
-Phone: <em> ".$user->__get('user_phone')." </em><br>
-Hours open to the public (0:00 - 24:00): <em> ".$user->__get('user_open_time')." </em><br>
-Exhibition area of ​​the gallery (m2): <em> ".$user->__get('user_gallery_comname')." </em><br>
-Year: <em>".$user->__get('user_area')." </em><br>
-Gallery Profile: <em> ".$user->__get('user_gallery_profile')."</em><br>
-Director Photo: <em>".$imagenDirector." </em><br>
-Director(s) Name: <em>".$user->__get('user_director_name')." </em><br>
-Director(s) Email: <em>".$user->__get('user_director_email')." </em><br>
-Contact Name: <em>".$user->__get('user_contact_name')." </em><br>
-Contact Email: <em>".$user->__get('user_contact_email')." </em><br>
+<strong>Image gallery</strong>: <em>$imagenGaleria</em><br>
+<strong>Gallery name</strong>: <em> ".$user->__get('user_gallery_comname')." </em><br>
+<strong>Gallery name</strong>: <em> ".$user->__get('user_gallery_razon')." </em><br>
+<strong>Document type</strong>: <em> ".$user->__get('user_document_type')." </em><br>
+<strong>Document number</strong>: <em> ".$user->__get('user_gallery_document')." </em><br>
+<strong>Country</strong>: <em>".$country->__get('country_id')."</em><br>
+<strong>City</strong>: <em> ".$user->__get('user_city')." </em><br>
+<strong>Address</strong>: <em> ".$user->__get('user_address')." </em><br>
+<strong>ZIP</strong>: <em> ".$user->__get('user_postal_code')." </em><br>
+<strong>Gallery Review</strong>: <em> ".$user->__get('user_gallery_comname')." </em><br>
+<strong>Phone</strong>: <em> ".$user->__get('user_phone')." </em><br>
+<strong>Hours open to the public (0:00 - 24:00)</strong>: <em> ".$user->__get('user_open_time')." </em><br>
+<strong>Exhibition area of ​​the gallery (m2)</strong>: <em> ".$user->__get('user_gallery_comname')." </em><br>
+<strong>Year</strong>: <em>".$user->__get('user_area')." </em><br>
+<strong>Gallery Profile</strong>: <em> ".$user->__get('user_gallery_profile')."</em><br>
+<strong>Director Photo:</strong> <em>".$imagenDirector." </em><br>
+<strong>Director(s) Name</strong>: <em>".$user->__get('user_director_name')." </em><br>
+<strong>Director(s) Email</strong>: <em>".$user->__get('user_director_email')." </em><br>
+<strong>Contact Name</strong>: <em>".$user->__get('user_contact_name')." </em><br>
+<strong>Contact Email</strong>: <em>".$user->__get('user_contact_email')." </em><br>
 </p>
 ";
 //EXPOSICIONES
@@ -39,9 +44,9 @@ foreach ($expositions as $exposition)
 {
 	$html	.= "
 	<p>
-	Name of the exhibition: <em>".$exposition->__get('exposition_name')."</em>,
-	Year: <em>".$exposition->__get('exposition_year')."</em>,
-	Month: <em>".$exposition->__get('exposition_month')."</em>,
+	<em>".$exposition->__get('exposition_name')."</em>,
+	<em>".$exposition->__get('exposition_year')."</em>,
+	<em>".$exposition->__get('exposition_month')."</em>
 	</p>";
 }
 //FERIAS
@@ -55,7 +60,7 @@ for ($i=0; $i < 8; $i++)
 $html	.= "<h3>Fairs</h3>";
 
 $html	.= "<p>
-Participation in artBO <em> ";
+<strong>Participation in artBO</strong> <em> ";
 	$html	.= ($artbo[6] == 1) ? "2012<br>" : '';
 	$html	.= ($artbo[0] == 1) ? "2011<br>" : '';
 	$html	.= ($artbo[1] == 1) ? "2010<br>" : '';
@@ -74,10 +79,10 @@ foreach ($ferias as $feria)
 	$country	= new Country($feria->__get('country_id'));
 	$html	.= "
 	<p><!--Esto por cada feria-->
-	Name of the Fair: <em>".$feria->__get('feria_name')."</em>,
-	City: <em>".$feria->__get('feria_city')."</em>,
-	Country: <em>".$country->__get('country_id')."</em>,
-	Year: <em>".$feria->__get('feria_year')." </em>,
+	<em>".$feria->__get('feria_name')."</em>,
+	<em>".$feria->__get('feria_city')."</em>,
+	<em>".$country->__get('country_id')."</em>,
+	<em>".$feria->__get('feria_year')." </em>,
 	</p>";
 }
 
@@ -89,7 +94,7 @@ $html	.= "<h3>Artist</h3>";
 $html	.= "
 
 <p>
-Artistic Proposal for artBO 2013: <em>".$user->__get('user_gallery_proposal')." </em>,
+<strong>Artistic Proposal for artBO 2013:</strong> <em>".$user->__get('user_gallery_proposal')." </em>,
 </p>";
 
 $html	.= "<h4>Represented Artists Proposed for artBO 2013</h4>";
@@ -101,12 +106,12 @@ for($i = 1; $i <= count($artists); $i++)
 	$artistWork	= ArtistWorkHelper::retrieveArtistWorks("AND artist_id = " . $artist->__get('artist_id'));
 $html	.= "
 	<p>
-	Name: <em> </em><br>
-	Last Name: <em> </em><br>
-	Nationality: <em> </em><br>
-	Date of birth: <em>".$artist->__get('artist_birthday')." </em><br>
-	Location: <em>". $artist->__get('artist_residency')." </em><br>
-	Artist Review: <em>".$artist->__get('artist_review')." </em><br>";
+	<strong>Name</strong>: <em> </em><br>
+	<strong>Last Name</strong>: <em> </em><br>
+	<strong>Nationality</strong>: <em> </em><br>
+	<strong>Date of birth</strong>: <em>".$artist->__get('artist_birthday')." </em><br>
+	<strong>Location</strong>: <em>". $artist->__get('artist_residency')." </em><br>
+	<strong>Artist Review</strong>: <em>".$artist->__get('artist_review')." </em><br>";
 
 	for($j=1; $j<=3; $j++)
 	{		
@@ -116,10 +121,10 @@ $html	.= "
 			
 			$file = file_exists($dir2 . $artistWork[($j - 1)]->__get('artist_work_file')) ? APPLICATION_FULL_URL . $dir2 . $artistWork[($j - 1)]->__get('artist_work_file') : '';
 			$html	.= "Work name: <em>".$artistWork[($j - 1)]->__get('artist_work_name')."</em><br>
-			Media: <em>".$artistWork[($j - 1)]->__get('artist_work_technique')."</em><br>
-			Dimensions: <em>".$artistWork[($j - 1)]->__get('artist_work_dimensions')."</em><br>
-			Year: <em>". $artistWork[($j - 1)]->__get('artist_work_year')."</em><br>
-			Image: <em>". $file."</em><br>";
+			<strong>Media</strong>: <em>".$artistWork[($j - 1)]->__get('artist_work_technique')."</em><br>
+			<strong>Dimensions</strong>: <em>".$artistWork[($j - 1)]->__get('artist_work_dimensions')."</em><br>
+			<strong>Year</strong>: <em>". $artistWork[($j - 1)]->__get('artist_work_year')."</em><br>
+			<strong>Image</strong>: <em>". $file."</em><br>";
 		}		
 	}
 	$html	. = "</p>";
@@ -152,8 +157,8 @@ $html	.= "<h3>Type of Stand</h3>";
 
 $html	.= "<p>
 
-Type of Stand: <em>".$stand."</em><br>
-Name with which you wish to appear on your stand: <em>".$user->__get('user_space_name')."</em>,
+<strong>Type of Stand</strong>: <em>".$stand."</em><br>
+<strong>Name with which you wish to appear on your stand</strong>: <em>".$user->__get('user_space_name')."</em>,
 </p>
 ";
 
@@ -162,10 +167,10 @@ Name with which you wish to appear on your stand: <em>".$user->__get('user_space
 $html	.= "<h3>Documents</h3>";
 
 $html	.= "<p>
-Certificate of incorporation: <em>".APPLICATION_FULL_URL.$dir.$user->__get('user_certificate')."</em><br>
-Tax Identification: <em>".APPLICATION_FULL_URL.$dir.$user->__get('user_rut')."</em><br>
-Identity Document: <em>".APPLICATION_FULL_URL.$dir.$user->__get('user_document')."</em><br>
-Copy of payment: <em>".APPLICATION_FULL_URL.$dir.$user->__get('user_payment')."</em><br>
+<strong>Certificate of incorporation</strong>: <em>".APPLICATION_FULL_URL.$dir.$user->__get('user_certificate')."</em><br>
+<strong>Tax Identification</strong>: <em>".APPLICATION_FULL_URL.$dir.$user->__get('user_rut')."</em><br>
+<strong>Identity Document</strong>: <em>".APPLICATION_FULL_URL.$dir.$user->__get('user_document')."</em><br>
+<strong>Copy of payment</strong>: <em>".APPLICATION_FULL_URL.$dir.$user->__get('user_payment')."</em><br>
 </p>
 ";
 
