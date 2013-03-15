acfacf27a6a7ce51ef817808d6ea1c82fae33d89
hugomarin <hm@hugomarin.com>
2013-03-15 18:30:00 -0500

    finalizar

--- en/view/finalizar.php
+++ en/view/finalizar.php
@@ -15,14 +15,14 @@ $dir		= 'resources/galerias/'. $user->__get('user_id'). '-' .  makeUrlClear(utf8
 				<div class="twelve columns">
 					<h3>Thank you for participating in <strong> ArtBo 2013.</strong></h3> 
 					
-					<p>You will receive a confirmation email of their registration process.</p>					
+					<p>The list of selected galleries will be published in <a href="http://www.artboonline.com" target="_blank">www.artboonline.com</a>.</p>					
 				</div>
 			</div>
 			<?php
 			echo '<h2>Your registry number is '.$user->__get('user_id').'</h2>';
             //require_once(SITE_VIEW.'endmail2.php');
             //echo utf8_decode($html);
-			echo '<p><a href="'.$dir.'finalizar.pdf">Download your registry info</a>';
+			echo '<h5><strong><a href="'.$dir.'finalizar.pdf">Download your registry info</a></strong>';
             ?>            
 		</div>
 		<div class="inner-footer">
