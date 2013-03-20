0fc871c9c19728f8153a3356ab7d576f0230dc88
Bernardo <bernardo@magdalenamedio.net>
2013-03-20 10:38:29 -0500

    ajuste

--- en/view/finalizar.php
+++ en/view/finalizar.php
@@ -22,7 +22,7 @@ $dir		= '../resources/galerias/'. $user->__get('user_id'). '-' .  makeUrlClear(u
 			echo '<h4>Your registry number is: '.$user->__get('user_id').'</h4>';
             //require_once(SITE_VIEW.'endmail2.php');
             //echo utf8_decode($html);
-			echo '<h5><strong><a href="'.$dir.'finalizar.pdf">Download your registry info</a></strong>';
+			echo '<h5><strong><a href="'.APPLICATION_IMAGE_DIR.'finalizar.pdf">Download your registry info</a></strong>';
             ?>            
 		</div>
 		<div class="inner-footer">
